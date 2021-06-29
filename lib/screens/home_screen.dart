b252c96009162ed9343c7daca9cb94733bc33bb9
Felipe Romo <felipe@romofel.dev>
2021-06-29 05:52:04 -0500

    Remove implicit padding and add our own

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,44 +9,48 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: CustomScrollView(
         slivers: [
-          SliverAppBar(
-            backgroundColor: Colors.transparent,
-            title: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Welcome back',
-                  style: AppTypography.montserrat.copyWith(
-                    color: AppTypography.darkColor,
-                    fontSize: 12,
+          SliverPadding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            sliver: SliverAppBar(
+              titleSpacing: 0,
+              backgroundColor: Colors.transparent,
+              title: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Welcome back',
+                    style: AppTypography.montserrat.copyWith(
+                      color: AppTypography.darkColor,
+                      fontSize: 12,
+                    ),
                   ),
-                ),
-                Text(
-                  'Jacob Jones',
-                  style: AppTypography.montserrat.copyWith(
-                    color: AppTypography.darkColor,
-                    fontSize: 18,
-                    fontWeight: FontWeight.w500,
+                  Text(
+                    'Jacob Jones',
+                    style: AppTypography.montserrat.copyWith(
+                      color: AppTypography.darkColor,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    ),
                   ),
-                ),
+                ],
+              ),
+              centerTitle: false,
+              actions: [
+                Container(
+                  width: 56,
+                  height: 56,
+                  alignment: Alignment.bottomCenter,
+                  decoration: BoxDecoration(
+                    color: AppPalette.avatarBackgroundColor,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Image.asset(
+                    'assets/images/home_screen/avatar@3x.png',
+                    scale: 1.1,
+                  ),
+                )
               ],
             ),
-            centerTitle: false,
-            actions: [
-              Container(
-                width: 56,
-                height: 56,
-                alignment: Alignment.bottomCenter,
-                decoration: BoxDecoration(
-                  color: AppPalette.avatarBackgroundColor,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Image.asset(
-                  'assets/images/home_screen/avatar@3x.png',
-                  scale: 1.1,
-                ),
-              )
-            ],
           ),
         ],
       ),
