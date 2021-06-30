faa5987c030bed1b12b1972fbdddef7a614051b9
Felipe Romo <felipe@romofel.dev>
2021-06-30 07:23:51 -0500

    Start styling the Search row TextField

--- lib/widgets/home_screen_bar.dart
+++ lib/widgets/home_screen_bar.dart
@@ -10,50 +10,63 @@ class HomeScreenBar extends SliverPersistentHeaderDelegate {
       child: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 24),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.start,
+          child: Column(
             children: [
-              Column(
-                // mainAxisAlignment: MainAxisAlignment.center,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    'Welcome back',
-                    style: AppTypography.montserrat.copyWith(
-                      color: AppTypography.darkColor,
-                      fontSize: 12,
-                    ),
+                  Column(
+                    // mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Welcome back',
+                        style: AppTypography.montserrat.copyWith(
+                          color: AppTypography.darkColor,
+                          fontSize: 12,
+                        ),
+                      ),
+                      Text(
+                        'Jacob Jones',
+                        style: AppTypography.montserrat.copyWith(
+                          color: AppTypography.darkColor,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      Text(
+                        '$shrinkOffset',
+                        style: AppTypography.montserrat.copyWith(
+                          color: AppTypography.darkColor,
+                        ),
+                      ),
+                    ],
                   ),
-                  Text(
-                    'Jacob Jones',
-                    style: AppTypography.montserrat.copyWith(
-                      color: AppTypography.darkColor,
-                      fontSize: 18,
-                      fontWeight: FontWeight.w500,
+                  Container(
+                    width: 56,
+                    height: 56,
+                    alignment: Alignment.bottomCenter,
+                    decoration: BoxDecoration(
+                      color: AppPalette.avatarBackgroundColor,
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                  ),
-                  Text(
-                    '$shrinkOffset',
-                    style: AppTypography.montserrat.copyWith(
-                      color: AppTypography.darkColor,
+                    child: Image.asset(
+                      'assets/images/home_screen/avatar@3x.png',
+                      scale: 1.1,
                     ),
-                  ),
+                  )
                 ],
               ),
-              Container(
-                width: 56,
-                height: 56,
-                alignment: Alignment.bottomCenter,
-                decoration: BoxDecoration(
-                  color: AppPalette.avatarBackgroundColor,
-                  borderRadius: BorderRadius.circular(10),
+              TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                  ),
                 ),
-                child: Image.asset(
-                  'assets/images/home_screen/avatar@3x.png',
-                  scale: 1.1,
-                ),
-              )
+              ),
             ],
           ),
         ),
