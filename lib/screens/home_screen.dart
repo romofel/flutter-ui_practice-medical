6af317ca30de1ff1895a4f469ebea362f28176c5
Felipe Romo <felipe@romofel.dev>
2021-06-30 07:11:15 -0500

    Use a SliverPersistentHeader instead of SliverAppBar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:app/constants.dart';
+import 'package:app/widgets/home_screen_bar.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -9,49 +10,60 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: CustomScrollView(
         slivers: [
-          SliverPadding(
-            padding: const EdgeInsets.symmetric(horizontal: 24),
-            sliver: SliverAppBar(
-              titleSpacing: 0,
-              backgroundColor: Colors.transparent,
-              title: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Welcome back',
-                    style: AppTypography.montserrat.copyWith(
-                      color: AppTypography.darkColor,
-                      fontSize: 12,
-                    ),
-                  ),
-                  Text(
-                    'Jacob Jones',
-                    style: AppTypography.montserrat.copyWith(
-                      color: AppTypography.darkColor,
-                      fontSize: 18,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                ],
-              ),
-              centerTitle: false,
-              actions: [
-                Container(
-                  width: 56,
-                  height: 56,
-                  alignment: Alignment.bottomCenter,
-                  decoration: BoxDecoration(
-                    color: AppPalette.avatarBackgroundColor,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Image.asset(
-                    'assets/images/home_screen/avatar@3x.png',
-                    scale: 1.1,
-                  ),
-                )
-              ],
-            ),
-          ),
+        SliverPersistentHeader(
+          pinned: true,
+          floating: false,
+          delegate: HomeScreenBar(),
+        ),
+          // SliverPadding(
+          //   padding: const EdgeInsets.symmetric(horizontal: 24),
+          //   sliver: SliverAppBar(
+          //     titleSpacing: 0,
+          //     backgroundColor: Colors.transparent,
+          //     title: Column(
+          //       crossAxisAlignment: CrossAxisAlignment.start,
+          //       children: [
+          //         Text(
+          //           'Welcome back',
+          //           style: AppTypography.montserrat.copyWith(
+          //             color: AppTypography.darkColor,
+          //             fontSize: 12,
+          //           ),
+          //         ),
+          //         Text(
+          //           'Jacob Jones',
+          //           style: AppTypography.montserrat.copyWith(
+          //             color: AppTypography.darkColor,
+          //             fontSize: 18,
+          //             fontWeight: FontWeight.w500,
+          //           ),
+          //         ),
+          //       ],
+          //     ),
+          //     centerTitle: false,
+          //     actions: [
+          //       Container(
+          //         width: 56,
+          //         height: 56,
+          //         alignment: Alignment.bottomCenter,
+          //         decoration: BoxDecoration(
+          //           color: AppPalette.avatarBackgroundColor,
+          //           borderRadius: BorderRadius.circular(10),
+          //         ),
+          //         child: Image.asset(
+          //           'assets/images/home_screen/avatar@3x.png',
+          //           scale: 1.1,
+          //         ),
+          //       )
+          //     ],
+          //     expandedHeight: 148,
+          //     flexibleSpace: FlexibleSpaceBar(
+          //       title: Text('Hello World', style: AppTypography.montserrat.copyWith(
+          //         color: AppTypography.darkColor,
+          //       )),
+          //     ),
+          //   ),
+          // ),
         ],
       ),
     );
