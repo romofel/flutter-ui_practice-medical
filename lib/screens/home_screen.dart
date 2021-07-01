67540b5ccf4e4746a1a2e709fc27527cd4f66239
Felipe Romo <felipe@romofel.dev>
2021-07-01 05:12:50 -0500

    Remove unused code

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -14,55 +14,6 @@ class HomeScreen extends StatelessWidget {
             floating: false,
             delegate: HomeScreenBar(),
           ),
-          // SliverPadding(
-          //   padding: const EdgeInsets.symmetric(horizontal: 24),
-          //   sliver: SliverAppBar(
-          //     titleSpacing: 0,
-          //     backgroundColor: Colors.transparent,
-          //     title: Column(
-          //       crossAxisAlignment: CrossAxisAlignment.start,
-          //       children: [
-          //         Text(
-          //           'Welcome back',
-          //           style: AppTypography.montserrat.copyWith(
-          //             color: AppTypography.darkColor,
-          //             fontSize: 12,
-          //           ),
-          //         ),
-          //         Text(
-          //           'Jacob Jones',
-          //           style: AppTypography.montserrat.copyWith(
-          //             color: AppTypography.darkColor,
-          //             fontSize: 18,
-          //             fontWeight: FontWeight.w500,
-          //           ),
-          //         ),
-          //       ],
-          //     ),
-          //     centerTitle: false,
-          //     actions: [
-          //       Container(
-          //         width: 56,
-          //         height: 56,
-          //         alignment: Alignment.bottomCenter,
-          //         decoration: BoxDecoration(
-          //           color: AppPalette.avatarBackgroundColor,
-          //           borderRadius: BorderRadius.circular(10),
-          //         ),
-          //         child: Image.asset(
-          //           'assets/images/home_screen/avatar@3x.png',
-          //           scale: 1.1,
-          //         ),
-          //       )
-          //     ],
-          //     expandedHeight: 148,
-          //     flexibleSpace: FlexibleSpaceBar(
-          //       title: Text('Hello World', style: AppTypography.montserrat.copyWith(
-          //         color: AppTypography.darkColor,
-          //       )),
-          //     ),
-          //   ),
-          // ),
         ],
       ),
     );
