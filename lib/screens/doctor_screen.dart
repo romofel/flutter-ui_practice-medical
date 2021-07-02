1b55e651c70b800df15196566e96491afbaabaf3
Felipe Romo <felipe@romofel.dev>
2021-07-02 08:29:15 -0500

    Style location

--- lib/screens/doctor_screen.dart
+++ lib/screens/doctor_screen.dart
@@ -72,11 +72,25 @@ class DoctorScreen extends StatelessWidget {
                   const SizedBox(height: 8),
                   const Padding(
                     padding: EdgeInsets.symmetric(horizontal: 24),
-                    child: Text('Specialist Dentist'),
+                    child: Text(
+                      'Specialist Dentist',
+                      style: TextStyle(
+                        color: Color(0xb302100F),
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
                   ),
                   const Padding(
                     padding: EdgeInsets.symmetric(horizontal: 24),
-                    child: Text('Massachusetts General Hospital, Boston, MA'),
+                    child: Text(
+                      'Massachusetts General Hospital, Boston, MA',
+                      style: TextStyle(
+                        color: Color(0x8002100F),
+                        fontSize: 12,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
                   ),
                   Row(),
                 ],
