21a8494fadb3e52ae9b6eff69268f49cf3054fdd
Felipe Romo <felipe@romofel.dev>
2021-07-05 11:06:22 -0500

    Scaffold stack for screen body

--- lib/screens/video_call_screen.dart
+++ lib/screens/video_call_screen.dart
@@ -5,6 +5,19 @@ class VideoCallScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                    'assets/images/video_call_screen/video_doctor_backdrop.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
