5dcb5badf271965af127d8d61fbf74db8fe405b0
Felipe Romo <felipe@romofel.dev>
2021-07-05 10:04:47 -0500

    Wrap appbar with padding for vertical alignment

--- lib/screens/appointment_screen.dart
+++ lib/screens/appointment_screen.dart
@@ -9,31 +9,34 @@ class AppointmentScreen extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: const Icon(
-                    Icons.arrow_back,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: const Icon(
+                      Icons.arrow_back,
+                      color: Color(0xff02100F),
+                    ),
+                  ),
+                  const Text(
+                    'Appointment',
+                    style: TextStyle(
+                      color: Color(0xff02100F),
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  Icon(
+                    Icons.more_vert_rounded,
                     color: Color(0xff02100F),
                   ),
-                ),
-                const Text(
-                  'Appointment',
-                  style: TextStyle(
-                    color: Color(0xff02100F),
-                    fontSize: 14,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-                Icon(
-                  Icons.more_vert_rounded,
-                  color: Color(0xff02100F),
-                ),
-              ],
+                ],
+              ),
             ),
           ],
         ),
