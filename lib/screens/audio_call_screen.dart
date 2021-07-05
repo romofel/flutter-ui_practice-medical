e126eaefdfdac0a8c1107dbc486b84fdff72b1c7
Felipe Romo <felipe@romofel.dev>
2021-07-05 14:08:19 -0500

    add audio call screen widget

---
+++ lib/screens/audio_call_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AudioCallScreen extends StatelessWidget {
+  const AudioCallScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+            children: [],
+        ),
+    );
+  }
+}
