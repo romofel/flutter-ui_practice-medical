8b37adb2a899e6d91d95359bc9e2e02ff4c41e7e
Felipe Romo <felipe@romofel.dev>
2021-07-05 09:59:05 -0500

    Use row instead of appbar widget

--- lib/screens/appointment_screen.dart
+++ lib/screens/appointment_screen.dart
@@ -6,35 +6,36 @@ class AppointmentScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0.0,
-        title: const Text(
-          'Appointment',
-          style: TextStyle(
-            color: Color(0xff02100F),
-            fontSize: 14,
-            fontWeight: FontWeight.w500,
-          ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                const Text(
+                  'Appointment',
+                  style: TextStyle(
+                    color: Color(0xff02100F),
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  icon: const Icon(
+                    Icons.arrow_back,
+                    color: Color(0xff02100F),
+                  ),
+                ),
+                Icon(
+                  Icons.more_vert_rounded,
+                  color: Color(0xff02100F),
+                ),
+              ],
+            ),
+          ],
         ),
-        leading: IconButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          icon: const Icon(
-            Icons.arrow_back,
-            color: Color(0xff02100F),
-          ),
-        ),
-        actions: const [
-          Icon(
-            Icons.more_vert_rounded,
-            color: Color(0xff02100F),
-          ),
-        ],
-      ),
-      body: const Center(
-        child: Text('Appointment Screen'),
       ),
     );
   }
