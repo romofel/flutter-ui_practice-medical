85d18e401455cf59769133994ff3d5e45869a131
Felipe Romo <felipe@romofel.dev>
2021-07-05 12:15:11 -0500

    Style background border and color and add padding

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -8,7 +8,42 @@ class ChatScreen extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          Row(),
+          Container(
+            padding: const EdgeInsets.only(top: 54, left: 24, right: 24),
+            decoration: BoxDecoration(
+              color: Color(0x80FFFFFF),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              ),
+            ),
+            child: Row(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Icon(
+                    Icons.arrow_back,
+                    color: Color(0xff02100F),
+                  ),
+                ),
+                Container(),
+                Column(
+                  children: [
+                    Text('Dr. Jenny Wilson'),
+                    Text('Dentist Specialist'),
+                  ],
+                ),
+                Container(
+                    child: Image.asset(
+                        'assets/images/chat_screen/appointment_phone_icon.png')),
+                Container(
+                    child: Image.asset(
+                        'assets/images/chat_screen/appointment_video_icon.png')),
+              ],
+            ),
+          ),
           Text('Today'),
           Expanded(
             child: Container(),
