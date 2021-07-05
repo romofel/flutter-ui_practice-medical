62b2edfe7775e6c82abac3be704f75cd819a3a81
Felipe Romo <felipe@romofel.dev>
2021-07-05 09:51:53 -0500

    Style the components in appbar

--- lib/screens/appointment_screen.dart
+++ lib/screens/appointment_screen.dart
@@ -9,18 +9,31 @@ class AppointmentScreen extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0.0,
-        title: Text('Appointment'),
+        title: const Text(
+          'Appointment',
+          style: TextStyle(
+            color: Color(0xff02100F),
+            fontSize: 14,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
         leading: IconButton(
           onPressed: () {
             Navigator.of(context).pop();
           },
-          icon: Icon(Icons.arrow_back),
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Color(0xff02100F),
+          ),
         ),
-        actions: [
-          Icon(Icons.more_vert_rounded),
+        actions: const [
+          Icon(
+            Icons.more_vert_rounded,
+            color: Color(0xff02100F),
+          ),
         ],
       ),
-      body: Center(
+      body: const Center(
         child: Text('Appointment Screen'),
       ),
     );
