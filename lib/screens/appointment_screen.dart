1a75c2275e4fae5c38451d857026891e75819d5c
Felipe Romo <felipe@romofel.dev>
2021-07-05 09:48:14 -0500

    Scaffold appbar content

--- lib/screens/appointment_screen.dart
+++ lib/screens/appointment_screen.dart
@@ -5,6 +5,22 @@ class AppointmentScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Appointment'),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: Icon(Icons.arrow_back),
+        ),
+        actions: [
+          Icon(Icons.more_vert_rounded),
+        ],
+      ),
+      body: Center(
+        child: Text('Appointment Screen'),
+      ),
+    );
   }
 }
