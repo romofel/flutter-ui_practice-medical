e5410a0c458f38811a02941291f2f71c5c1d3cfa
Felipe Romo <felipe@romofel.dev>
2021-07-05 12:49:01 -0500

    Add horizontal padding to keep content alignment

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -101,24 +101,27 @@ class ChatScreen extends StatelessWidget {
             child: Container(),
           ),
           SafeArea(
-            child: Row(
-              children: [
-                Container(
-                  width: 46,
-                  height: 46,
-                  decoration: BoxDecoration(
-                    color: Color(0x1a20BEB8),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Center(
-                    child: Image.asset(
-                      'assets/images/chat_screen/send_icon.png',
-                      fit: BoxFit.cover,
-                      width: 18,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Row(
+                children: [
+                  Container(
+                    width: 46,
+                    height: 46,
+                    decoration: BoxDecoration(
+                      color: Color(0x1a20BEB8),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Center(
+                      child: Image.asset(
+                        'assets/images/chat_screen/send_icon.png',
+                        fit: BoxFit.cover,
+                        width: 18,
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ],
